17429c70066a7b0dd01a0c5d3cd24218724c1425
alexandra.feldman@rockfeather.com <alexandra.feldman@rockfeather.com>
2026-08-07 20:53:20 +0000

    remove normalizer

--- training/TrainWithFeatureStore.py
+++ training/TrainWithFeatureStore.py
@@ -130,9 +130,7 @@ mlflow.end_run()
 # Start an mlflow run, which is needed for the feature store to log the model
 mlflow.start_run()
 
-# Since the rounded timestamp columns would likely cause the model to overfit the data
-# unless additional feature engineering was performed, exclude them to avoid training on them.
-exclude_columns = []
+exclude_columns = ["year"]
 
 fe = FeatureEngineeringClient()
 
@@ -163,7 +161,7 @@ training_df.display()
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, Normalizer
+from sklearn.preprocessing import StandardScaler
 
 features_and_label = training_df.columns
 
@@ -171,8 +169,8 @@ features_and_label = training_df.columns
 data = training_df.toPandas()[features_and_label]
 
 train, test = train_test_split(data, random_state=123)
-X_train = train.drop(["population", "year"], axis=1)
-X_test = test.drop(["population", "year"], axis=1)
+X_train = train.drop(["population"], axis=1)
+X_test = test.drop(["population"], axis=1)
 y_train = train.population
 y_test = test.population
 
@@ -181,12 +179,14 @@ mlflow.sklearn.autolog()
 #reg.score(X_test, y_test) #what is 
 
 # Train a Linear Regression model
-model_pipeline = Pipeline([("scaler", StandardScaler()), ("normalize", Normalizer()), ("model", Ridge())])
+model_pipeline = Pipeline([("scaler", StandardScaler()), ("model", Ridge())])
 model = model_pipeline.fit(X_train, y_train)
 
 # COMMAND ----------
 
 # DBTITLE 1, Log model and return output.
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 # Log the trained model with MLflow and package it with feature lookup information.
 fe.log_model(
@@ -199,7 +199,7 @@ fe.log_model(
 
 #Log metrics with mlflow 
 mlflow.log_metric("r2_score", model.score(X_test, y_test))
-mlflow.log_metric("mse", mean_squared_error(y_test, model.predict(X_test)))
+mlflow.log_metric("mae", mean_absolute_error(y_test, model.predict(X_test)))
 mlflow.log_metric("rmse", mean_squared_error(y_test, model.predict(X_test), squared=False))
 
 # The returned model URI is needed by the model deployment notebook.
